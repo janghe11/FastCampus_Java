366e1b464f1366b543ee053d3d4de95b43c3db64
Taehee Jang <jang0913@gmail.com>
2018-11-18 19:39:31 +0900

    Test추가, Entity 업데이트

---
+++ Course/20181112/stickermall/src/test/java/net/thjang/blog/stickermall/repository/ExpireTypeRepositoryTests.java
@@ -0,0 +1,41 @@
+package net.thjang.blog.stickermall.repository;
+
+import net.thjang.blog.stickermall.domain.ExpireType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Transactional
+public class ExpireTypeRepositoryTests {
+    @Autowired
+    private ExpireTypeRepository expireTypeRepository;
+
+    @Test
+	public void Coupon목록All가져오기() {
+        List<ExpireType> expireTypeList = expireTypeRepository.findAll();
+
+        Assert.assertNotNull(expireTypeList);
+        for (ExpireType expireType : expireTypeList) {
+
+        }
+    }
+
+    @Test
+    public void Coupon목록Name으로검색해서최근Date순으로정렬() {
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<ExpireType> expireTypeRepositories = expireTypeRepository.findAllByName("장태희", pageable);
+
+
+    }
+}
